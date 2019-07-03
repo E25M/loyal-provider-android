9d3098fa26f38cfdf23683086e8b4f87849703ad
nadusha92 <nadusha9271@gmail.com>
2019-07-03 14:39:44 +0530

    share count not updating bug fix

---
+++ app/src/main/java/pet/loyal/provider/HomeScreen.kt
@@ -0,0 +1,12 @@
+package pet.loyal.provider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_home_screen)
+    }
+}
