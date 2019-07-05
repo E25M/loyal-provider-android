2eb21ff6c2055a561cf48bdeda3e31f2f3ff9cf4
amila-rangana <amila@eight25media.com>
2019-07-05 11:26:01 +0530

    initial dev

---
+++ app/src/main/java/pet/loyal/provider/view/splash/SplashActivity.kt
@@ -0,0 +1,15 @@
+package pet.loyal.provider.view.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pet.loyal.provider.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_parent_self_invite)
+
+
+    }
+}
