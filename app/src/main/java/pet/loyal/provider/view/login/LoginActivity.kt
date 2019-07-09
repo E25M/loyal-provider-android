70fa91d7aaa956deb5327ce99519384f00bf874e
nadusha92 <nadusha9271@gmail.com>
2019-07-09 11:14:23 +0530

    Merge branch 'master' of https://github.com/rehanfernando/Loyal-Provider-Android
    # Conflicts:
    #	app/fabric.properties

---
+++ app/src/main/java/pet/loyal/provider/view/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package pet.loyal.provider.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pet.loyal.provider.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
